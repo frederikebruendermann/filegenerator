1fca20123cd05380f76acb82a058bdf1494dbe03
Frederike Bründermann <82148810+frederikebruendermann@users.noreply.github.com>
2021-05-26 15:49:46 +0200

    create functions for all three options

--- index.js
+++ index.js
@@ -1,18 +1,54 @@
 const writeFile = require('./writeFile')
 const inquirer = require('inquirer')
 
+const templates = {
+  component: name =>
+    `
+    import styled from 'styled-components/macro'
+      
+    export default function ${name} () {
+        return <div>${name}</div>
+    }
+          `,
+  spec: name =>
+    `
+    import {render, screen} from '@testing-library/react'
+    import ${name} from './${name}'
+      
+    describe('${name}', () => {
+        it('renders', () => {
+            render(<${name} />)
+            expect(screen.getByText('${name}')).toBeInTheDocument()
+        })
+    })
+          `,
+  stories: name =>
+    `
+    import ${name} from './${name}'
+    export default {
+        title: '${name}',
+        component: ${name}
+    }
+      
+    const Template = args => <${name} {...args} />
+      
+    export const Default = Template.bind({})
+    Default.args = {}
+          `,
+}
+
 const questions = [
   {
     type: 'input',
-    name: 'file',
+    name: 'name',
     message:
-      'Which functions and files would you like to create? Please seperate by comma.',
+      'Which functions and files would you like to create? Plase seperate by comma.',
   },
   {
     type: 'checkbox',
-    message: 'Please select a file type',
+    message: 'Plase select a file type',
     name: 'fileTypes',
-    choices: [{ name: 'component' }, { name: 'Spec' }, { name: 'Stories' }],
+    choices: [{ name: 'component' }, { name: 'spec' }, { name: 'stories' }],
     validate(answer) {
       if (answer.length < 1) {
         return 'You must choose at least one file type.'
@@ -23,7 +59,14 @@ const questions = [
 ]
 
 inquirer.prompt(questions).then(answers => {
-  const files = answers['file'].split(',').map(el => el.trim())
-  files.forEach(file => writeFile(file))
-  console.log(answers)
+  // answers = { name: 'Header', fileTypes: ['component', 'spec']}
+
+  // iterate over fileTypes array from answer
+  answers.fileTypes.forEach(fileType => {
+    // get value (our template function) from each fileType
+    const templateFunction = templates[fileType]
+    // call function and save string
+    const fileString = templateFunction(answers.name)
+    writeFile(answers.name, fileType, fileString)
+  })
 })
