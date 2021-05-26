c8d9709ba3ccdd70f065ca47ecec944f46c81725
Frederike Bründermann <82148810+frederikebruendermann@users.noreply.github.com>
2021-05-26 12:57:05 +0200

    import inquirer to get an questions array

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const writeFile = require('./writeFile')
 const readline = require('readline')
+const inquirer = require('inquirer')
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+const questions = [
+  {
+    type: 'input',
+    name: 'file',
+    message:
+      'Which functions and files would you like to create? Please seperate by comma.',
+  },
+]
+
+inquirer.prompt(questions).then(answers => {
+  const files = answers['file'].split(',').map(el => el.trim())
+  files.forEach(file => writeFile(file))
+  console.log(files)
 })
-
-rl.question(
-  'Which functions and files would you like to create? Please seperate by comma.',
-  answer => {
-    answer.split(',').map(name => writeFile(name.trim()))
-    rl.close()
-  }
-)
