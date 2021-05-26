32cbef2806179b41506fec5e3dd9820fbae83df7
Frederike Bründermann <82148810+frederikebruendermann@users.noreply.github.com>
2021-05-26 14:36:44 +0200

    add second question incl checkboxen

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const writeFile = require('./writeFile')
-const readline = require('readline')
 const inquirer = require('inquirer')
 
 const questions = [
@@ -9,10 +8,22 @@ const questions = [
     message:
       'Which functions and files would you like to create? Please seperate by comma.',
   },
+  {
+    type: 'checkbox',
+    message: 'Please select a file type',
+    name: 'fileTypes',
+    choices: [{ name: 'component' }, { name: 'Spec' }, { name: 'Stories' }],
+    validate(answer) {
+      if (answer.length < 1) {
+        return 'You must choose at least one file type.'
+      }
+      return true
+    },
+  },
 ]
 
 inquirer.prompt(questions).then(answers => {
   const files = answers['file'].split(',').map(el => el.trim())
   files.forEach(file => writeFile(file))
-  console.log(files)
+  console.log(answers)
 })
